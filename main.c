6ac67c39eeab9eb40a015f32f8167fa304358060
mrpoor <greg15001@gmail.com>
2019-09-29 03:13:15 -0400

    Fixed up some bugs add travis yaml file

--- main.c
+++ main.c
@@ -4,18 +4,19 @@
 #include <string.h>
 #include "dictionary.h"
 
+#define DICTIONARY "wordlist.txt"
+
 extern hashmap_t * hashtable[];
 int main()
 {
-	bool success = load_dictionary("test_worlist.txt", hashtable);
-	print_hashtable(hashtable);
-//	if (success){
-//		char cwd[PATH_MAX];
-//		getcwd(cwd, sizeof(cwd));
-//		strcat(cwd, "/test1.txt");
-//		FILE *fp = fopen(cwd, "r");
-//		char* misspell[256];
-//		check_words(fp, hashtable, misspell);
-//	}
-//	return 0;
+	bool success = load_dictionary("wordlist.txt", hashtable);
+	if (success){
+		char cwd[PATH_MAX];
+		getcwd(cwd, sizeof(cwd));
+		strcat(cwd, "/test1.txt");
+		FILE *fp = fopen(cwd, "r");
+		char* misspell[MAX_MISSPELLED];
+		check_words(fp, hashtable, misspell);
+	}
+	return 0;
 }
--- spell.c
+++ spell.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
+#include <ctype.h>
 #include "dictionary.h"
+
 
 
 
@@ -12,41 +14,59 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
 	size_t line_buf_size = 0;
 	int line_count = 0;
 	ssize_t line_size;
-	/* The problem with this is I think we can get oom by big files? */
+	int misspell_count = 0;
+	// The problem with this is I think we can get oom by big files?
 	line_size = getline(&line_buf, &line_buf_size, fp);
 	while (line_size >= 0)
 	{
-		line_count++;
-		line_size = getline(&line_buf, &line_buf_size, fp);
+		line_buf[strcspn(line_buf, "\n")] = 0;
 		char * pch;
-		pch = strtok(line_buf," ,.-");
+		pch = strtok(line_buf," ,.;:");
 		while (pch != NULL)
 		{
-			check_word(pch, hashtable);
-			pch = strtok(NULL, " ,.-");
+			bool is_word_spelled = check_word(pch, hashtable);
+			if (!(is_word_spelled)){
+				printf("Found misspelled word %s\n", pch);
+				misspelled[misspell_count] = pch;
+				misspell_count++;
+			}
+			pch = strtok(NULL, " ,.;:");
 		}
+		line_count++;
+		line_size = getline(&line_buf, &line_buf_size, fp);
 	}
+//	print_misspell(misspelled);
 	free(line_buf);
 	line_buf = NULL;
 	fclose(fp);
-	return 1;
+	return misspell_count;
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]){
-//	int hash_code = hash_function(tolower(word));
-//	printf("the word is %s\n", word);
-//	printf("the hashcode is %d\n", hash_code);
-//	node* word_node = hashtable[hash_code];
-//	if (word_node != NULL){
-//		node seen_node_value = *word_node;
-//		printf("found one %s\n", seen_node_value.word);
-//		printf("the end of found\n");
+	int hash_code = hash_function(word);
+	hashmap_t word_node_ptr = hashtable[hash_code];
+	printf("the hashcode of word %s is %d\n", word, hash_code);
+	while(word_node_ptr != NULL)
+	{
+		if (strcmp(word_node_ptr->word, word) == 0){
+			printf("word found %s\n", word_node_ptr->word);
+			return true;
+		}
+		word_node_ptr = word_node_ptr->next;
+	}
+//	char * lower_word = strlwr_custom(word);
+//	int hash_code_lower = hash_function(lower_word);
+//	hashmap_t word_node_ptr_lower = hashtable[hash_code_lower];
+//	printf("the hashcode of lower word %s is %d\n", lower_word, hash_code_lower);
+//	while(word_node_ptr_lower != NULL)
+//	{
+//		if (strcmp(word_node_ptr_lower->word, lower_word) == 0){
+//			printf("word found lower case\n");
+//			return true;
+//		}
+//		word_node_ptr_lower = word_node_ptr_lower->next;
 //	}
-//	return false;
-	int hash_code = 539;
-	node* blah = hashtable[hash_code];
-	node blah_value = *blah;
-	printf("looked at word in hashtable %s\n", blah_value.word);
+	return false;
 }
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
@@ -59,59 +79,74 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 	int line_count = 0;
 	ssize_t line_size;
 	line_size = getline(&line_buf, &line_buf_size, fp);
-	hashmap_t first_node_ptr = malloc(sizeof(hashmap_t));
-	node first_node = *first_node_ptr;
-	line_buf[strcspn(line_buf, "\n")] = 0;
-	strcpy(first_node.word, line_buf);
-	printf("putting word into hashtable %s\n", first_node.word);
-	int hash_code = hash_function(line_buf);
-	printf("the hashcode is %d\n", hash_code);
-	hashtable[hash_code] = &first_node;
 	while(line_size != -1){
+		hashmap_t new_node_ptr = calloc(1, sizeof(node));
+		line_buf[strcspn(line_buf, "\n")] = 0;
+		strcpy(new_node_ptr->word, line_buf);
+		int hash_code = hash_function(line_buf);
+		new_node_ptr->next = NULL;
+		if (hashtable[hash_code] != NULL){
+			insert_node_at_end(new_node_ptr, hashtable[hash_code]);
+		}else{
+			hashtable[hash_code] = new_node_ptr;
+		}
 		line_count++;
 		line_size = getline(&line_buf, &line_buf_size, fp);
-		printf("There is a word and it is %s\n", line_buf);
-		hashmap_t new_node_ptr = malloc(sizeof(hashmap_t));
-		node new_node = *new_node_ptr;
-		new_node.next = NULL;
-		line_buf[strcspn(line_buf, "\n")] = 0;
-		strcpy(new_node.word, line_buf);
-		printf("putting word into hashtable %s\n", new_node.word);
-		int hash_code = hash_function(line_buf);
-		printf("the hashcode is %d\n", hash_code);
-		if (hashtable[hash_code] != NULL){
-			printf("entered none null hashcode\n");
-			break;
-//			node* seen_node = hashtable[hash_code];
-//			node seen_node_value = *seen_node;
-//			while (seen_node_value.next != NULL){
-//				seen_node_value = *seen_node_value.next;
-//			}
-//			seen_node_value.next = &new_node;
-		}else{
-			hashtable[hash_code] = &new_node;
-		}
-		node* blah = hashtable[hash_code];
-		node blah_value = *blah;
-		printf("looked at word in hashtable %s\n", blah_value.word);
 	}
-//	int hash_code2 = 636;
-//	node* blah = hashtable[hash_code2];
-//	node blah_value = *blah;
-//	printf("looked at word in hashtable %s\n", blah_value.word);
 	return true;
 }
 
 
+void insert_node_at_end(hashmap_t new_node, hashmap_t head)
+{
+	hashmap_t temp = malloc(sizeof(hashmap_t));
+	if(new_node == NULL)
+	{
+		printf("Unable to allocate memory.");
+	}
+	else
+	{
+		temp = head;;
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+}
+
+
 void print_hashtable(hashmap_t hashtable[]){
-	for(int i; i<1000; i++){
-		node* blah = hashtable[i];
-		if (blah != NULL){
-			node blah_value = *blah;
-			char word = blah_value.word;
-			printf("the hashcode was %d and the word is %s\n", i, blah_value.word);
-			printf("the node pointer is %p\n", &blah_value);
-			printf("the word pointer is %p\n", &word);
+	printf("starting printing hashtable\n");
+	for(int i=0; i<HASH_SIZE; i++){
+		hashmap_t blah = hashtable[i];
+		while(blah != NULL){
+			printf("the hashcode is %d\n", i);
+			printf("the word is %s, next %p\n", blah->word, blah->next);
+			blah = blah->next;
 		}
 	}
 }
+
+void print_hashtable_one_link(hashmap_t hashtable[], int hash){
+	hashmap_t blah = hashtable[hash];
+	printf("link list %p, hash %d", blah, hash);
+	while(blah != NULL) {
+		printf("the hashcode is %d\n", hash);
+		printf("the word is %s, next %p\n", blah->word, blah->next);
+		blah = blah->next;
+	}
+}
+
+////Took it from here: https://ftp.samba.org/pub/unpacked/lorikeet-heimdal/lib/roken/strlwr.c
+//strlwr_custom(char *str)
+//{
+//	printf("running tolower\n");
+//	char *s;
+//	for(s = str; *s; s++){
+//		printf("to_lower before %d", (int)*s);
+//		*s = tolower(*s);
+//		printf("to_lower after %d", (int)*s);
+//	}
+//	return str;
+//}
