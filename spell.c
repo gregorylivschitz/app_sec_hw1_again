9034af79179ec65128c69fb75bbefb1bcec74fa1
mrpoor <greg15001@gmail.com>
2019-09-29 14:05:38 -0400

    Fixed up some bugs deallocate memory

--- spell.c
+++ spell.c
@@ -15,21 +15,22 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
 	int line_count = 0;
 	ssize_t line_size;
 	int misspell_count = 0;
+	bool max_misspell = false;
 	// The problem with this is I think we can get oom by big files?
 	line_size = getline(&line_buf, &line_buf_size, fp);
-	while (line_size >= 0)
+	while (line_size >= 0 && misspell_count < MAX_MISSPELLED)
 	{
 		line_buf[strcspn(line_buf, "\n")] = 0;
 		char * pch;
-		pch = strtok(line_buf," ,.;:");
-		while (pch != NULL)
+		pch = strtok(line_buf," ,.;:\n\t\r\"");
+		while (pch != NULL && misspell_count < MAX_MISSPELLED)
 		{
 			bool is_word_spelled = check_word(pch, hashtable);
 			if (!(is_word_spelled)){
 				misspelled[misspell_count] = pch;
 				misspell_count++;
 			}
-			pch = strtok(NULL, " ,.;:");
+			pch = strtok(NULL, " ,.;:\n\t\r\"");
 		}
 		line_count++;
 		line_size = getline(&line_buf, &line_buf_size, fp);
@@ -37,30 +38,33 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
 	free(line_buf);
 	line_buf = NULL;
 	fclose(fp);
+	deallocate_hashtable(hashtable);
 	return misspell_count;
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]){
 	int hash_code = hash_function(word);
 	hashmap_t word_node_ptr = hashtable[hash_code];
-	while(word_node_ptr != NULL)
-	{
-		if (strcmp(word_node_ptr->word, word) == 0){
-			return true;
+	if (hash_code >= 0) {
+
+
+		while (word_node_ptr != NULL) {
+			if (strcmp(word_node_ptr->word, word) == 0) {
+				return true;
+			}
+			word_node_ptr = word_node_ptr->next;
 		}
-		word_node_ptr = word_node_ptr->next;
-	}
-	char lower_word[LENGTH+1];
-	strcpy(lower_word, word);
-	str_custom(lower_word);
-	int hash_code_lower = hash_function(lower_word);
-	hashmap_t word_node_ptr_lower = hashtable[hash_code_lower];
-	while(word_node_ptr_lower != NULL)
-	{
-		if (strcmp(word_node_ptr_lower->word, lower_word) == 0){
-			return true;
+		char lower_word[LENGTH + 1];
+		strcpy(lower_word, word);
+		str_custom(lower_word);
+		int hash_code_lower = hash_function(lower_word);
+		hashmap_t word_node_ptr_lower = hashtable[hash_code_lower];
+		while (word_node_ptr_lower != NULL) {
+			if (strcmp(word_node_ptr_lower->word, lower_word) == 0) {
+				return true;
+			}
+			word_node_ptr_lower = word_node_ptr_lower->next;
 		}
-		word_node_ptr_lower = word_node_ptr_lower->next;
 	}
 	return false;
 }
@@ -75,7 +79,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 	int line_count = 0;
 	ssize_t line_size;
 	line_size = getline(&line_buf, &line_buf_size, fp);
-	while(line_size != -1){
+	while(line_size >= 0){
 		hashmap_t new_node_ptr = calloc(1, sizeof(node));
 		line_buf[strcspn(line_buf, "\n")] = 0;
 		strcpy(new_node_ptr->word, line_buf);
@@ -89,6 +93,8 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 		line_count++;
 		line_size = getline(&line_buf, &line_buf_size, fp);
 	}
+	free(line_buf);
+	line_buf = NULL;
 	fclose(fp);
 	return true;
 }
@@ -133,5 +139,18 @@ void str_custom(char * temp) {
 	while (*s) {
 		*s = tolower((unsigned char) *s);
 		s++;
+	}
+}
+
+void deallocate_hashtable(hashmap_t hashtable[]){
+	for(int i=0; i<HASH_SIZE; i++){
+		hashmap_t node = hashtable[i];
+		hashmap_t temp;
+		while(node != NULL){
+			temp = node->next;
+			free(node);
+			node = temp;
+		}
+		hashtable[i] = NULL;
 	}
 }
